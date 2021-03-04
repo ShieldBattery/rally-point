840f64ef76f40c6a39f3a9b2b1f4a4286939ad32
Travis Collins <travis@tec27.com>
2021-03-04 03:03:57 -0800

    Dockerize the server.

---
+++ Dockerfile
@@ -0,0 +1,22 @@
+FROM node:14-alpine as builder
+ENV NODE_ENV=production
+
+RUN npm install -g npm@7.x.x
+
+# Set the working directory to the home directory of the `node` user
+WORKDIR /home/node/rallypoint
+
+RUN chown node:node .
+
+# Copy the whole repository to the image, *except* the stuff marked in the `.dockerignore` file
+COPY --chown=node:node . .
+
+# Set the user to `node` for any subsequent `RUN` and `CMD` instructions
+USER node
+
+RUN npm install
+
+# rally-point protocol port
+EXPOSE 14098/udp
+
+CMD node ./index.js
--- app.js
+++ app.js
@@ -2,20 +2,28 @@ import nconf from 'nconf'
 import path from 'path'
 import createServer from './create-server'
 
-nconf.env()
+nconf.env({ lowerCase: true })
 nconf.argv()
 nconf.file(path.join(__dirname, 'config.json'))
 nconf.defaults({
-  host: '::1',
-  port: 14098,
+  // eslint-disable-next-line camelcase
+  rp_host: '::1',
+  // eslint-disable-next-line camelcase
+  rp_port: 14098,
 })
 
 nconf.required(['secret'])
 
-const server = createServer(nconf.get('host'), nconf.get('port'), nconf.get('secret'))
+const server = createServer(nconf.get('rp_host'), nconf.get('rp_port'), nconf.get('secret'))
 setInterval(() => {
   console.log(`${server.numRoutes} routes active`)
 }, 5 * 60 * 1000)
-server.bind().then(() => {
-  console.log(`listening on ${server.host}:${server.port}`)
-})
+server.bind().then(
+  () => {
+    console.log(`listening on ${server.host}:${server.port}`)
+  },
+  err => {
+    console.error('Error: ' + err + '\n' + err.stack)
+    process.exit(1)
+  },
+)
